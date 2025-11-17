e345c3e7f0e3ad94dc9a5713e25d54f5bdbda967
sathvikr59 <skarkala2024@fau.edu>
2025-11-16 23:26:06 -0500

    Delete evaluate.py

--- evaluate.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--weights', required=True)
-parser.add_argument('--data', required=True)
-args = parser.parse_args()
-model = YOLO(args.weights)
-results = model.val(data=args.data)
-print(results)
